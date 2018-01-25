3a040ccde6a028824040f9a9b6e466e8216ca50a
lauranor <laura.paajanen@helsinki.fi>
2018-01-25 23:17:29 +0200

    1.5

--- vko1/osa1/src/index.js
+++ vko1/osa1/src/index.js
@@ -2,28 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-    const kurssi = 'Half Stack -sovelluskehitys'
-    const osat = [
-        {
-          nimi: 'Reactin perusteet',
-          tehtavia: 10
-        },
-        {
-          nimi: 'Tiedonvälitys propseilla',
-          tehtavia: 7
-        },
-        {
-          nimi: 'Komponenttien tila',
-          tehtavia: 14
-        }
-      ]
-    
+      const kurssi = {
+        nimi: 'Half Stack -sovelluskehitys',
+        osat: [
+          {
+            nimi: 'Reactin perusteet',
+            tehtavia: 10
+          },
+          {
+            nimi: 'Tiedonvälitys propseilla',
+            tehtavia: 7
+          },
+          {
+            nimi: 'Komponenttien tila',
+            tehtavia: 14
+          }
+        ]
+      }
 
   return (
     <div>
-      <Otsikko name={kurssi} />
-      <Sisalto osat={osat} /> 
-      <Yhteensa count={osat} />
+      <Otsikko course={kurssi} />
+      <Sisalto course={kurssi} /> 
+      <Yhteensa course={kurssi} />
     </div>
   )
 }
@@ -31,22 +32,22 @@ const App = () => {
 const Sisalto = (props) => {
     return(
         <div>
-            <p> {props.osat[0].nimi} {props.osat[0].tehtavia} </p>
-            <p> {props.osat[1].nimi} {props.osat[1].tehtavia} </p>
-            <p> {props.osat[2].nimi} {props.osat[2].tehtavia} </p>
+            <p> {props.course.osat[0].nimi} {props.course.osat[0].tehtavia} </p>
+            <p> {props.course.osat[1].nimi} {props.course.osat[1].tehtavia} </p>
+            <p> {props.course.osat[2].nimi} {props.course.osat[2].tehtavia} </p>
         </div>
     )
 }
 
 const Otsikko = (props) => {
     return(
-        <h1> {props.name} </h1>
+        <h1> {props.course.nimi} </h1>
     )
 }
 
 const Yhteensa = (props) => {
     return(
-        <p>yhteensä {props.count[0].tehtavia + props.count[1].tehtavia + props.count[2].tehtavia} tehtävää</p>
+        <p>yhteensä {props.course.osat[0].tehtavia + props.course.osat[1].tehtavia + props.course.osat[2].tehtavia} tehtävää</p>
     )
 }
 
