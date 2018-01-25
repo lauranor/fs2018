570cd21c8e75b1e55d9e54d20651d9697784b023
lauranor <laura.paajanen@helsinki.fi>
2018-01-25 23:12:51 +0200

    1.3

--- vko1/osa1/src/index.js
+++ vko1/osa1/src/index.js
@@ -2,21 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  const kurssi = 'Half Stack -sovelluskehitys'
-  const osa1 = 'Reactin perusteet'
-  const tehtavia1 = 10
-  const osa2 = 'Tiedonvälitys propseilla'
-  const tehtavia2 = 7
-  const osa3 = 'Komponenttien tila'
-  const tehtavia3 = 14
+    const kurssi = 'Half Stack -sovelluskehitys'
+    const osa1 = {
+      nimi: 'Reactin perusteet',
+      tehtavia: 10
+    }
+    const osa2 = {
+      nimi: 'Tiedonvälitys propseilla',
+      tehtavia: 7
+    }
+    const osa3 = {
+      nimi: 'Komponenttien tila',
+      tehtavia: 14
+    }
 
   return (
     <div>
       <Otsikko name={kurssi} />
-      <Sisalto num={osa1} teht={tehtavia1} />
-      <Sisalto num={osa2} teht={tehtavia2} />
-      <Sisalto num={osa3} teht={tehtavia3} />
-      <Yhteensa count={tehtavia1 + tehtavia2 + tehtavia3} />
+      <Sisalto num={osa1.nimi} teht={osa1.tehtavia} />
+      <Sisalto num={osa2.nimi} teht={osa2.tehtavia} />
+      <Sisalto num={osa3.nimi} teht={osa3.tehtavia} />
+      <Yhteensa count={osa1.tehtavia + osa2.tehtavia + osa3.tehtavia} />
     </div>
   )
 }
